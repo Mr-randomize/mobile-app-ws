270eabefd0d9be9a6027f55969312e85639eb02c
ivi.berberi <iviberberi@gmail.com>
2020-01-23 23:41:30 +0100

    Native SQL Queries Test

--- src/main/java/com/iviberberi/app/ws/io/repositories/UserRepository.java
+++ src/main/java/com/iviberberi/app/ws/io/repositories/UserRepository.java
@@ -1,13 +1,28 @@
 package com.iviberberi.app.ws.io.repositories;
 
 import com.iviberberi.app.ws.io.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long> {
 
     UserEntity findByEmail(String email);
+
     UserEntity findByUserId(String userId);
+
     UserEntity findUserByEmailVerificationToken(String token);
+
+    @Query(value = "select * from Users u where u.EMAIL_VERIFICATION_STATUS = 'true'",
+            countQuery = "select count(*) from Users u where u.EMAIL_VERIFICATION_STATUS = 'true'",
+            nativeQuery = true)
+    Page<UserEntity> findAllUsersWithConfirmedEmailAddress(Pageable pageableRequest);
+
+    @Query(value = "select * from Users u where u.FIRST_NAME = ?1", nativeQuery = true)
+    List<UserEntity> findUserByFirstName(String firstName);
 }
